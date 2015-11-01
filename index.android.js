59517bf61ca4dacfe550ff29223ea6d39d2a765f
Aakash N S <aakashns1@gmail.com>
2015-11-02 01:47:14 +0530

    Reset index.android.js

--- index.android.js
+++ index.android.js
@@ -12,9 +12,6 @@ var {
   View,
 } = React;
 
-var { DialogAndroid } = React.NativeModules;
-console.log(DialogAndroid);
-
 var ReactNativeDialogsExample = React.createClass({
   render: function() {
     return (
